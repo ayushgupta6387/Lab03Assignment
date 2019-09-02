2b7a2fac0acbdfde9361f928305ffab35e24f0d5
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-09-01 22:35:08 -0700

    print the output

--- src/main/Money.java
+++ src/main/Money.java
@@ -6,7 +6,11 @@ public class Money {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double money = scanner.nextDouble();
-
+        String convert = Double.toString(money);
+        int index = convert.indexOf('.');
+        String m = convert.substring(0, index);
+        String n = convert.substring(index + 1, convert.length());
+        System.out.println(m + "Rupees and " + n + "Paise");
     }
 
     }
