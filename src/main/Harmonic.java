4512d73cc50417d5c4999b374af2635ec06cc60e
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-09-01 22:11:14 -0700

    take 'n' as input

--- src/main/Harmonic.java
+++ src/main/Harmonic.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Harmonic {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double n = scanner.nextDouble();
+        double sum = 0;
+
 
 
     }
