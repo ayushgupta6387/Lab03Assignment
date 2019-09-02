6ae9da3dc4056d84319b679cdf4d2a695ca969a0
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-09-01 23:10:15 -0700

    take input as 'fahrenheit'

--- src/main/Converter.java
+++ src/main/Converter.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Converter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double fahrenheit = scanner.nextDouble();
 
+    }
     }
 }
