2bc606b35825098a9571de89c6560684a91d36f5
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-09-01 23:32:57 -0700

    use nested for loop

--- src/main/Pattern.java
+++ src/main/Pattern.java
@@ -1,7 +1,8 @@
 package main;
-
 public class Pattern {
     public static void main(String[] args) {
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 1; j <= i; j++) {
 
-    }
+            }
 }
