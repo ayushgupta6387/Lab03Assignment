8f8fad5d0dc45889c7ae2c234647df54b521af98
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-09-01 22:18:13 -0700

    take 'money' as input

--- src/main/Money.java
+++ src/main/Money.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Money {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double money = scanner.nextDouble();
 
     }
-}
+
+    }
+
