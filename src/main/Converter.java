0efd48d67bb45b64f52ad3eabd14ad72bf644325
AyushGupta <ayush.gupta1cs18@gla.ac.in>
2019-09-01 23:20:46 -0700

    import 'DecimalFormat'

--- src/main/Converter.java
+++ src/main/Converter.java
@@ -1,12 +1,14 @@
 package main;
 
+import java.text.DecimalFormat;
 import java.util.Scanner;
-
 public class Converter {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double fahrenheit = scanner.nextDouble();
+        double celsius = 0.0;
+        DecimalFormat obj = new DecimalFormat("#.######");
 
     }
     }
-}
+
